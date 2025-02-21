561ae8b6cc296ecc526422d9e2b5418cd90e9aa1
INN <ivngigiy@gmail.com>
2025-02-21 14:33:43 +0300

    add a new app relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
 
-
+from . import views
+urlpatterns = [
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.register, name='register'),
+    path(views.register, "LogoutView.as_view(template_name="), 
+    path(views.register, "LoginView.as_view(template_name=")
+]
