b99ed14c0c9397bbdfa3ec31da239d68469635b0
INN <ivngigiy@gmail.com>
2025-02-21 14:24:36 +0300

    add a new app relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import Book, Library, Author
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView      
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
 
 def list_books(request):
     books = Book.objects.all()  
@@ -19,3 +24,15 @@ def add_author(name):
     else:
         print(f"Author '{name}' already exists.")
        
+
+#UserCreationFormform and the CreateViewclass-based view to handle user registration.
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+#utilities for handling user login and logout processes.
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/',LogoutView.as_view(), name= 'logout'),
+]
