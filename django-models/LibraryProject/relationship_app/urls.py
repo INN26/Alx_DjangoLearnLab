df15479960ea5695ac7bab54ec830e7f94f97eb9
INN <ivngigiy@gmail.com>
2025-02-21 12:24:01 +0300

    add a new app relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import LibraryDetailView, book_list
+from .views import LibraryDetailView,list_books
 from .views import list_books
 
 urlpatterns = [
-    path('books/', book_list, name='book_list'),
+    path('books/', list_books, name='book_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
 
