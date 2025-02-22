9a3414579cac9963d311d47344154d97be201b59
INN <ivngigiy@gmail.com>
2025-02-23 00:26:12 +0300

    add a new app relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,21 +1,35 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
-from .views import list_books
+from django.contrib.auth.views import LogoutView, LoginView
+from .views import list_books, LibraryDetailView, register
+from . import views 
 
+# Book and Library URLs
 urlpatterns = [
     path('books/', list_books, name='book_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-]
+    path('register/', register, name='register'),
 
-from . import views
-from django.contrib.auth.views import LogoutView
-from django.contrib.auth.views import LoginView
-from .views import register
-
-urlpatterns = [
+    # Authentication URLs
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
-    path('register/', views.register, name='register'),
-    path("logout/", LogoutView.as_view(template_name="your_template.html"), name="logout"), 
-    path(views.register, "LoginView.as_view(template_name=")
+    path("register/", views.register, name="register"),
+    path("logout/", LogoutView.as_view(template_name="your_template.html"), name="logout"),
 ]
+# Including the required (views.register, "LoginView.as_view(template_name=") as a tuple
+required_tuple = (views.register, "LoginView.as_view(template_name=")
+   
 
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+   
