d8c02a7a3e38151b1401bde44faa8357a23b70d4
INN <ivngigiy@gmail.com>
2025-03-21 15:02:08 +0300

    Implementing the Blog's User Authentication System

--- django_blog/staticfiles/accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.utils.timezone import now  # Import Django's timezone utility
-
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(default=now)  # Use `default=now` instead of `auto_now_add=True`
-    updated_at = models.DateTimeField(auto_now=True)
--- django_blog/staticfiles/accounts/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def login_view(request):
-    return render(request, 'accounts/login.html')
-
-def logout_view(request):
-    pass
