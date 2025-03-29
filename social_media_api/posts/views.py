0bbd1f264affa1b077fc1e998f71a8dd2f52055b
INN <ivngigiy@gmail.com>
2025-03-29 12:38:04 +0300

    Implementing User Follows and Feed Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -38,5 +38,5 @@ class UserFeedView(ListAPIView):
     def get_queryset(self):
         """Retrieve posts from followed users."""
         user = self.request.user
-        followed_users = user.following.all()
-        return Post.objects.filter(author__in=followed_users).order_by("-created_at")
+        following_users = user.following.all()
+        return Post.objects.filter(author__in= following_users ).order_by('-created_at')
